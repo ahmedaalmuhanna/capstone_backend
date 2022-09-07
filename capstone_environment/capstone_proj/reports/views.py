83f50b860822a29750ae3cfd15e63fe4038dbfe8
Ahmed <ahmed.a.almuhanna@hotmail.com>
2022-09-07 18:00:53 +0300

    6:00 9-7-2022.. solving the creating report problem

--- capstone_environment/capstone_proj/reports/views.py
+++ capstone_environment/capstone_proj/reports/views.py
@@ -47,6 +47,8 @@ class IOCDeleteView(DestroyAPIView):
 #Create report
 class ReportCreateView(CreateAPIView):
     serializer_class = ReportSerializer
+    queryset = Report.objects.all()
+
 
 
 #List All Reports
